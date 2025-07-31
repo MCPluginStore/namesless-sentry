3a6d4a1530ba4687533baebe0842b585e2dc6d93
Enno Gelhaus <egelhaus@ennogelhaus.de>
2025-08-01 00:46:43 +0200

    error logs

--- init.php
+++ init.php
@@ -1,4 +1,9 @@
 <?php
+// Enable error reporting for debugging
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+<?php
 require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/module.php';
 
